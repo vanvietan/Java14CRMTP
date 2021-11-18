57597946fb8ca5610bab1f1bb12400568e3c6a65
vanvietan <vanvietan2911@gmail.com>
2021-11-18 15:11:55 -0800

    1st-commit
    kết nối database crm_app thành công

--- src/main/java/cybersoft/backend/java14/crm/servlet/HomeServlet.java
+++ src/main/java/cybersoft/backend/java14/crm/servlet/HomeServlet.java
@@ -13,5 +13,6 @@ public class HomeServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("WEB-INF/views/home.jsp").forward(req, resp);
+		
 	}
 }
