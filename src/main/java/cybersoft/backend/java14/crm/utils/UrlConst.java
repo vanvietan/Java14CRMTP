3ae6a8baf2eaacbf66b93285feedebdcf6b52c0f
vanvietan <vanvietan2911@gmail.com>
2021-11-19 11:19:49 -0800

    fix-loi-login-servlet
    fix lỗi login servlet

--- src/main/java/cybersoft/backend/java14/crm/utils/UrlConst.java
+++ src/main/java/cybersoft/backend/java14/crm/utils/UrlConst.java
@@ -1,6 +1,10 @@
 package cybersoft.backend.java14.crm.utils;
 
 public class UrlConst {
+	
+	final public static String HEALTH="/health";
+	final public static String DB_CHECK="/db-check";
+	
 	final public static String ROOT = "/*";
 	final public static String USERLIST = "/userlist";
 	final public static String HOME = "/home";
