29479bb6181d08ca0e4ca209a14b1c49fd56ad0e
raymustang <91603977+raymustang@users.noreply.github.com>
2021-12-03 20:40:22 +0700

    3rd summit

--- src/main/java/cybersoft/backend/java14/crm/servlet/TaskServlet.java
+++ src/main/java/cybersoft/backend/java14/crm/servlet/TaskServlet.java
@@ -1,12 +1,17 @@
 package cybersoft.backend.java14.crm.servlet;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 
 import cybersoft.backend.java14.crm.model.Task;
+import cybersoft.backend.java14.crm.model.User;
 import cybersoft.backend.java14.crm.service.TaskService;
+import cybersoft.backend.java14.crm.service.UserService;
 import cybersoft.backend.java14.crm.util.JspConst;
 import cybersoft.backend.java14.crm.util.UrlConst;
 import javax.servlet.ServletException;
@@ -17,19 +22,24 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet (name="taskServlet", urlPatterns= {
 		UrlConst.TASK_UPDATE_ASSIGNEE,
 		UrlConst.TASK_DELETE,
-		UrlConst.TASK_LIST
+		UrlConst.TASK_LIST,
+		UrlConst.TASK_ADD,
+		UrlConst.TASK_SHOW_LIST_USER
 })
 public class TaskServlet extends HttpServlet {
 	private String action;
 	private TaskService service;
 	private Task task;
 	private int userId, taskId;
+	private String sd, ed;
+	private UserService userService;
 
 	@Override
 	public void init() throws ServletException {
 		super.init();
 		service = new TaskService();
 		task = new Task();
+		userService = new UserService();
 	}
 
 	@Override
@@ -41,7 +51,13 @@ public class TaskServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		switch (action) {
-
+		
+		/* ADD task */
+		case UrlConst.TASK_ADD:
+			req.getRequestDispatcher(JspConst.TASK_ADD)
+			.forward(req, resp);
+			break;
+			
 		/* SHOW LIST task */
 		case UrlConst.TASK_LIST:
 			List<Task> tasks = service.getTasks();
@@ -58,15 +74,49 @@ public class TaskServlet extends HttpServlet {
 			resp.sendRedirect(req.getContextPath() + UrlConst.TASK_LIST);
 			break;
 			
-		/* DELETE task*/
+		/* DELETE task */
 		case UrlConst.TASK_DELETE:
 			taskId = Integer.parseInt(req.getParameter("taskId")) ;
 			service.deleteTask(taskId);
 			resp.sendRedirect(req.getContextPath() + UrlConst.TASK_LIST);
 			break;
-
+		
+		/* SHOW LIST USER */
+		case UrlConst.TASK_SHOW_LIST_USER:
+			taskId = Integer.parseInt(req.getParameter("taskId"));
+			List<User> users = userService.getUsers();
+			req.setAttribute("taskId", taskId);
+			req.setAttribute("users", users);
+			req.getRequestDispatcher(JspConst.TASK_SHOW_LIST_USER)
+			.forward(req, resp);
+			break;
+		
 		default:
 			break;
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		if ("create" != null) {
+			task.setName(req.getParameter("name"));
+			task.setDescription(req.getParameter("description"));
+			sd = req.getParameter("start_date").replace("-","");
+			ed = req.getParameter("end_date").replace("-","");
+			
+			LocalDate start_date = LocalDate.parse(sd, DateTimeFormatter.BASIC_ISO_DATE);
+			LocalDate end_date = LocalDate.parse(ed, DateTimeFormatter.BASIC_ISO_DATE);
+				
+			java.sql.Date sqlsd = Date.valueOf(start_date);
+			java.sql.Date sqled = Date.valueOf(end_date);
+				
+			task.setStart_date(sqlsd);
+			task.setEnd_date(sqled);
+			task.setAssignee(1);
+			task.setStatus(3);
+			task.setProject(1);
+			service.addTask(task);
+			resp.sendRedirect(req.getContextPath() + UrlConst.TASK_LIST);
 		}
 	}
 }
